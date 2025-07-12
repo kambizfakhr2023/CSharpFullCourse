e33fda92b3d1739578dc1708a38d7415a718d532
Kambiz <kambiz.fakhr@yahoo.com>
2025-07-12 11:02:39 +0330

    kambiz do this

--- ConsoleApp1/Soheil.cs
+++ ConsoleApp1/Soheil.cs
@@ -1,17 +1,1 @@
-﻿using System;
-
-public class Class1
-{
-	public Class1()
-	{
-
-        private int myVar;
-
-    public int MyProperty
-    {
-        get { return myVar; }
-        set { myVar = value; }
-    }
-
-}
-}
+﻿// this is first test for pull and push
