0efc49bc42b04d1548b2e7312b763b2d5f59d602
Kambiz <kambiz.fakhr@yahoo.com>
2025-07-12 11:48:24 +0330

    Variables

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-//test
-Console.WriteLine("Hello, World!");
+﻿byte myByteNum = 10;
+
+Console.WriteLine(myByteNum);
+Console.WriteLine(default(byte));
+Console.WriteLine(byte.MaxValue);
+Console.WriteLine(byte.MinValue);
+
+
+Console.ReadKey();
