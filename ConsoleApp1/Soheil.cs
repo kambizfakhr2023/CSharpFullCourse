ca3616dbf6e390acfcd1bcac22c4e7c4ca248f5b
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-07-12 10:58:14 +0330

    my first push

---
+++ ConsoleApp1/Soheil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+        private int myVar;
+
+    public int MyProperty
+    {
+        get { return myVar; }
+        set { myVar = value; }
+    }
+
+}
+}
