60f6e9fc0d145561ed3f1c997253f9a214758494
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-07-12 12:33:08 +0330

    test01

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,27 @@
-﻿byte myByteNum = 10;
+﻿
+byte myByteNum = 10;
+int myIntNum = 20;
+char myCharNum = 'Z';
+double myDoubleNum = 16;
 
 Console.WriteLine(myByteNum);
 Console.WriteLine(default(byte));
 Console.WriteLine(byte.MaxValue);
 Console.WriteLine(byte.MinValue);
 
+Console.WriteLine(myIntNum);
+Console.WriteLine(default(int));
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(int.MinValue);
+
+Console.WriteLine(myCharNum);
+Console.WriteLine(default(char));
+Console.WriteLine(char.MaxValue);
+Console.WriteLine(char.MinValue);
+
+Console.WriteLine(myDoubleNum);
+Console.WriteLine(default(double));
+Console.WriteLine(double.MaxValue);
+Console.WriteLine(double.MinValue);
 
 Console.ReadKey();
