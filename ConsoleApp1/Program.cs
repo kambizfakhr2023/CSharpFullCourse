b4057eccd546bc5d9aa6230c88f9eb97608d1f76
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-07-19 11:33:12 +0330

    Loops 02 or 03

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,32 @@
 ﻿// Loops
+
+int number = 1;
+string result = "Hello";
+char character = 'o';
+int j = 9;
+int k = 0;
+
+while (number <= 10)
+{
+    Console.WriteLine(number);
+    number++;
+}
+
+do
+{
+    result += character;
+} while (result.Length < 25);
+
+Console.WriteLine(result);
+
+for (; j >= 0; j--)
+{
+    Console.WriteLine(j);
+}
+
+for (; k <= 10; k++)
+{
+    if ((k == 6) || (k == 7))
+        continue;
+    Console.WriteLine(k);
+}
