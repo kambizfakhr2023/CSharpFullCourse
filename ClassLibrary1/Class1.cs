d82534e43650713410cc4fa754085a8736b8bebe
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-09-06 12:09:59 +0330

    Null Types

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,30 +1,6 @@
 ﻿
 
-public class Emplpoyee
+public class Person
 {
-    public int Salary;
-}
-
-public class Student
-{
-    public int Marks;
-}
-
-public class Sample
-{
-    public void PrintData<T>(T obj) where T : class
-    {
-        if (obj.GetType() == typeof(Student))
-        {
-            Student temp = obj as Student;
-            Console.WriteLine(temp.Marks);
-        }
-        else if (obj.GetType() == typeof(Emplpoyee))
-        {
-            Emplpoyee temp = obj as Emplpoyee;
-            Console.WriteLine(temp.Salary);
-        }
-    }
-
-
+    public int? NoOfChildren;
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,25 @@
 ﻿
 
-Sample sample = new Sample();
-Emplpoyee emp = new Emplpoyee() { Salary = 1000 };
-Student stu = new Student() { Marks = 80 };
+Person p1 = new Person() { NoOfChildren = 2 };
+Person p2 = new Person() { NoOfChildren = null };
+Person p3 = new Person() { NoOfChildren = null };
 
-sample.PrintData<Emplpoyee>(emp);
-sample.PrintData<Student>(stu);
+Console.WriteLine(p1.NoOfChildren);
+
+//With If block
+//if (p2.NoOfChildren.HasValue)
+//{
+//    int x = p2.NoOfChildren.Value;
+//    Console.WriteLine(x);
+//}
+
+//With trinary
+//Console.WriteLine((p2.NoOfChildren.HasValue) ? p2.NoOfChildren.Value : 0);
+
+//With coalescing
+Console.WriteLine(p2.NoOfChildren ?? 0);
+
+//With propagation
+//Console.WriteLine(p2?.NoOfChildren);
 
 Console.ReadKey();
