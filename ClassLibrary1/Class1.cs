f58faa6c863dd3cbfc868eaf947a4423e72b21f2
FranzCantPickAName <soheil.rasouly@gmail.com>
2025-09-06 10:59:15 +0330

    Multiple Generics

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary1
+{
+    public class User<T1, T2>
+    {
+
+        public T1 RegistrationStatus;
+
+        public T2 Age;
+
+        
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,21 @@
-﻿// test
+﻿
+
+using ClassLibrary1;
+using System.Security.Principal;
+
+User<int, int> user1  = new User<int, int>();
+User<bool, string> user2 = new User<bool, string>();
+
+user1.RegistrationStatus = 1234;
+user2.RegistrationStatus = false;
+
+user1.Age = 22;
+user2.Age = "35 - 40";
+
+Console.WriteLine(user1.RegistrationStatus);
+Console.WriteLine(user2.RegistrationStatus);
+
+Console.WriteLine(user1.Age);
+Console.WriteLine(user2.Age);
+
+Console.ReadKey();
